e52bf140b4d3599410149320e5ff2dfe177cabcb
Abhijit Barman <141611515+abhickkb@users.noreply.github.com>
2025-09-25 08:42:31 +0530

    Add files via upload

---
+++ images/logic.js
@@ -0,0 +1,65 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+
+
+// document.getElementById('resume').addEventListener('click', function () {
+//   // Open the PDF in a new tab
+//   window.open('/images/full stack web devloper resume.pdf', '_blank');
+
+//   // Trigger the download
+//   const a = document.createElement('a');
+//   a.href = '/images/full stack web devloper resume.pdf';
+//   a.download = 'full stack web devloper resume.pdf';
+//   a.click();
+// })
+
+document.getElementById('resume').addEventListener('click', function (event) {
+  event.preventDefault();
+  
+  window.open('images/Abhijit CV.pdf', '_blank')
+  
+  const a = document.createElement('a')
+  
+  a.href = 'images/Abhijit CV.pdf';
+  
+  a.download = 'computer.pdf';
+  
+  document.body.appendChild(a);
+  
+  a.click();
+  
+  document.body.removeChild(a);
+});
+
+
